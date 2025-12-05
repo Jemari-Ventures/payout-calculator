557e72fac5d3dccd8222637b844c33e6261ac457
umarizulkifli <umarizulkifli@gmail.com>
2025-12-06 04:29:09 +0800

    Update pickup fee to 151.00

--- management.py
+++ management.py
@@ -74,7 +74,7 @@ class Config:
             {"min": 10, "max": 30, "rate": 2.70},
             {"min": 30, "max": float('inf'), "rate": 4.00}
         ],
-        "pickup_fee": 0.00,
+        "pickup_fee": 151.00,
         "currency_symbol": "RM",
         "penalty_rate": 100.0
     }
